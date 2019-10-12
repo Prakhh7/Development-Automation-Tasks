ccef6edecab8198734eae29208d5eea9af2c4801
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-10-12 12:44:08 +0530

    problem 6

--- WHATSAPP_QUESTIONS/problem6.sh
+++ WHATSAPP_QUESTIONS/problem6.sh
@@ -1,8 +1,8 @@
 #bin/bash
 
-echo " *                  *"
-echo " *  *            *     *"
-echo " *     *      *           *"
-echo " *     *      *           *"
-echo " *  *            *     *"
-echo " *                  *"
+echo " *****        ****"
+echo " *    *      *    *"
+echo " *    *      *    *"
+echo " *    *      *    *"
+echo " *    *      *    *"
+echo " *****        ****"
