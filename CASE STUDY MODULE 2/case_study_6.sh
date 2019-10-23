b4e3b6dcd22a4750c737cb53f2c192b886df25f1
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-10-23 16:45:57 +0530

    case study 6 modified

--- CASE STUDY MODULE 2/case_study_6.sh
+++ CASE STUDY MODULE 2/case_study_6.sh
@@ -3,7 +3,7 @@ read -e -p "Enter the command to check : " com
 if [ $(command -v $com) ];
 then
 echo "Command Found"
-echo "Type of Command is $(type -p $command)"
+echo "Type of Command is $(type -p $com)"
 else
 echo "Command not Found"
 fi
